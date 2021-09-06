fec924e65a72989621e1fd4803d2d366f1c7ecd0
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-09-06 21:13:17 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+> Simples Chat App using flask and flasksocketio
+
+# Setup
+1. Install dependencies
+```bash
+$ pip install -r requirements.txt
+```
+
+2. Run the server
+```bash
+$ python src/server.py
+```
+
+3. Navigate to http://localhost:5000
---
+++ src/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oywpQyaUqh8qXY8NcNqzLbcAT51ntD"
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username != "":
+            return render_template('chat.html', username=username)
+    return render_template('index.html')
+
+@socketio.on('joined_room')
+def handle_new_connection(username):
+    emit('joined_announcement', username, broadcast=True)
+
+@socketio.on('new_message')
+def handle_new_message(data):
+    emit('broadcast_message', data, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app)
